1e783e65cde5f9e3e56dd68c696e3119764eddb4
Annalisa Castaldo <acastald@c2r1p4.42firenze.it>
2026-06-22 18:44:34 +0200

    battle.py

--- milestone2/python_modules/python07/battle.py
+++ milestone2/python_modules/python07/battle.py
@@ -1,5 +1,14 @@
-from ex0 import CreatureFactory, FlameFactory, AquaFactory
+from ex0.factory import CreatureFactory, FlameFactory, AquaFactory
+
+
+def testing(factory: CreatureFactory) -> None:
+    print("Testing factory")
+    
+
+
+def fight(flame_factory: FlameFactory, aqua_factory: AquaFactory) -> None:
+    pass
 
 
 if __name__ == "__main__":
-    
+    testing(FlameFactory())
