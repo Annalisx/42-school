bbd04d1f08d53f5ea3632ed2e56e9914f6debf7e
Annalisa Castaldo <acastald@c2r1p11.42firenze.it>
2026-07-23 18:48:27 +0200

    codex

--- milestone3/codexion/codexion.h
+++ milestone3/codexion/codexion.h
@@ -6,7 +6,7 @@
 /*   By: acastald <acastald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:37:23 by acastald          #+#    #+#             */
-/*   Updated: 2026/07/22 11:13:19 by acastald         ###   ########.fr       */
+/*   Updated: 2026/07/23 17:59:19 by acastald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,27 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <pthread.h>
+# include <string.h>
 
-typedef struct s_list
+typedef struct s_coders
 {
-	int				number_of_coders;
+	int				coder_id;
 	long			time_to_burnout;
 	long			time_to_compile;
 	long			time_to_debug;
 	long			time_to_refactor;
 	int				number_of_compiles_required;
 	long			dongle_cooldown;
-	struct s_list	*scheduler;
-	struct s_list	*next;
-	struct s_list	*prev;
-}	t_list;
+	int				algo;
+	struct s_coders	*next;
+	struct s_coders	*prev;
+}	t_coders;
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+static int	count_words(char c, char const *s);
+static void	free_all(char **matrix, int j);
+static int	fill_matrix(char **matrix, char const *s, char c);
+char	**ft_split(char const *s, char c);
+
 
 #endif
--- milestone3/codexion/main.c
+++ milestone3/codexion/main.c
@@ -6,20 +6,40 @@
 /*   By: acastald <acastald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 15:25:14 by acastald          #+#    #+#             */
-/*   Updated: 2026/07/22 11:07:34 by acastald         ###   ########.fr       */
+/*   Updated: 2026/07/23 18:46:31 by acastald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+
 int	main(int ac, char **av)
 {
-	t_list	value;
-
+	t_coders type;
+	
+	if (ac == 2)
+	{
+		printf("\x1b[31mInvalid number of arguments\x1b[31m\n");
+		printf("\033[0mEach argument must be space-separated or enclosed in double quotes.\033[0m\n");
+		return (1);
+	}
 	if (ac != 9)
 	{
-		printf("Invalid number of arguments");
+		printf("Invalid number of arguments\n");
 		return (1);
+	}
+	if (((strcmp(av[8], "fifo")) != 0) || ((strcmp(av[8], "edf")) != 0))
+	{
+		if ((strcmp(av[8], "fifo")) == 0)
+			type.algo = 0;
+		else if ((strcmp(av[8], "edf")) == 0)
+			type.algo = 1;
+		else
+		{
+			printf("The value must be exactly one of: fifo or edf.\n");
+			return (1);
+		}
+		printf("%d\n", type.algo);
 	}
 	return (0);
 }
